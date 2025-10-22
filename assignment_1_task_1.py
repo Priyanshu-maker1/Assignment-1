4a35aeb89720a3a3b6274569dc7df7df2f269132
Priyanshu-maker1 <a.priyanshu6@gmail.com>
2025-10-22 20:18:28 +0530

    initial commit

---
+++ assignment_1_task_1.py
@@ -0,0 +1,12 @@
+s1=int(input("Enter the 1st number"))
+s2=int(input("Enter the 2nd number"))
+a=s1+s2
+s=s1-s2
+m=s1*s2
+n=s1/s2
+print("Addition =",a)
+print("Subtraction =",s)
+print("Multiplication =",m)
+print("Division =",n)
+
+
